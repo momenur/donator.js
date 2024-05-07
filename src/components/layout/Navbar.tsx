64d125cad29fb1005736b1308277240aa48db4ea
momenur <momenurislam6@gmail.com>
2024-05-07 21:35:16 +0600

    added initial setup and router setup

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <ul className="flex gap-4">
+        <li>Home</li>
+        <li>Donations</li>
+        <li>Dashboard</li>
+        <li>Login</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/routes/routes.tsx
@@ -0,0 +1,41 @@
+import App from "@/App";
+import AdminLayout from "@/components/layout/AdminLayout";
+import About from "@/pages/About";
+import Page1 from "@/pages/Dashboard/Page1";
+import Page2 from "@/pages/Dashboard/Page2";
+import Home from "@/pages/Home";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+
+  {
+    path: "/admin",
+    element: <AdminLayout />,
+    children: [
+      {
+        index: true,
+        element: <Page1 />,
+      },
+      {
+        path: "page2",
+        element: <Page2 />,
+      },
+    ],
+  },
+]);
+
+export default router;
