daba2d1d0d186033060f21413eac7129f156d503
momenur <momenurislam6@gmail.com>
2024-05-11 23:21:59 +0600

    added type script

--- src/pages/Dashboard/PieGraph.tsx
+++ src/pages/Dashboard/PieGraph.tsx
@@ -54,7 +54,7 @@ const PieGraph = () => {
           fill="#8884d8"
           dataKey="value"
         >
-          {data.map((entry, index) => (
+          {data.map((_, index) => (
             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
           ))}
         </Pie>
